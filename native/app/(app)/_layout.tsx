a64b992de24d42c9462561fbbf90f70313b83f00
kevin-griley <kevin@griley.app>
2025-03-28 08:36:19 -0700

    Adding page titles

--- native/app/(app)/_layout.tsx
+++ native/app/(app)/_layout.tsx
@@ -1,5 +1,7 @@
 import { LoadingScreen } from '@/components/LoadingScreen';
 import { useSession } from '@/components/SessionProvider';
+import { ThemeToggle } from '@/components/ThemeToggle';
+import { H4 } from '@/components/ui/typography';
 
 import { Redirect, Stack } from 'expo-router';
 
@@ -14,5 +16,30 @@ export default function AppLayout() {
         return <Redirect href="/" />;
     }
 
-    return <Stack />;
+    return (
+        <Stack
+            screenOptions={{
+                headerTitle(props) {
+                    return <H4>{toOptions(props.children)}</H4>;
+                },
+                headerRight: () => <ThemeToggle />,
+            }}
+        />
+    );
+}
+
+function toOptions(name: string) {
+    switch (name) {
+        default: {
+            const title = name
+                .split('-')
+                .map(function (str: string) {
+                    return str.replace(/\b\w/g, function (char) {
+                        return char.toUpperCase();
+                    });
+                })
+                .join(' ');
+            return title;
+        }
+    }
 }
